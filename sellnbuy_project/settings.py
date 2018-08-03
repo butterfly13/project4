3b69049a93bffca6f430769cf865239dd4e0eda3
Noon <noon@Noons-MacBook-Pro.local>
2018-08-03 12:13:54 -0400

    debug

--- sellnbuy_project/settings.py
+++ sellnbuy_project/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-
+# import dj-database-url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+# db_from_env = dj_database_url.config()
+# DATABASES[‘default’].update(db_from_env)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
