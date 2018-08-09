fecb6668ce45148acf64ae464b3bd201627b9e5f
Noon <noon@Noons-MacBook-Pro.local>
2018-08-09 11:31:16 -0400

    comment out heroku setup

--- sellnbuy_project/settings.py
+++ sellnbuy_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -147,4 +147,4 @@ MEDIA_URL = '/media/'
 # MEDIA_ROOT = 'media/images'
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
